df5de53be8715b2b47e475f0fb3199c72a71a321
Nayan <n007kingkong@gmail.com>
2025-07-13 14:33:03 +0600

    value insertion using math.random function

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 
-var marks = [56,60,76,53,34,87,91];
+var marks = [56,60,76,53,34];
 
 console.log(marks);
 
@@ -9,5 +9,11 @@ console.log(math);
 var position = marks.indexOf(34);
 console.log(position);
  
+marks[0] = Math.round(Math.random()*100); 
+marks[1] = Math.round(Math.random()*100); 
+marks[2] = Math.round(Math.random()*100); 
+marks[3] = Math.round(Math.random()*100); 
+marks[4] = Math.round(Math.random()*100); 
 
+console.log(marks);
 
